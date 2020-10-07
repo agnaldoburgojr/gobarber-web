e24e56316c6a7bc223ee8003d1f9266f7b7f8211
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-10-07 11:15:09 -0300

    feat: add private routes

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  console.log('addhdhdhd');
+  return (
+    <div>
+      <h1>ddhdhdhdh</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
