98ca965a5c1ba90d285f8676ec3c632a3fe94062
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-10-20 14:33:15 -0300

    feat: add dashboard header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
+import { Container, Header, HeaderContent, Profile } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+import { FiPower } from 'react-icons/fi';
+import { useAuth } from '../../hooks/auth';
+
 const Dashboard: React.FC = () => {
-  console.log('addhdhdhd');
+  const { signOut, user } = useAuth();
+
   return (
-    <div>
-      <h1>ddhdhdhdh</h1>
-    </div>
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem-vindo</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={() => signOut()}>
+            <FiPower></FiPower>
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
   );
 };
 
