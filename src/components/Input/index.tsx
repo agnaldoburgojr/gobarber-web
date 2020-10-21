e5d999348f029fc56ee7472eaa6ef778d0e5d860
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-10-21 10:22:11 -0300

    feat: add profile page

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -12,10 +12,16 @@ import { Container, Error } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
+  containerStyle?: object;
   icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+const Input: React.FC<InputProps> = ({
+  containerStyle = {},
+  name,
+  icon: Icon,
+  ...rest
+}) => {
   const inputRef = useRef<HTMLInputElement>(null);
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -39,7 +45,12 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   }, [fieldName, registerField]);
 
   return (
-    <Container isFocused={isFocused} isFilled={isFilled} isErrored={!!error}>
+    <Container
+      style={containerStyle}
+      isFocused={isFocused}
+      isFilled={isFilled}
+      isErrored={!!error}
+    >
       {Icon && <Icon size={20} />}
       <input
         onFocus={handleInputFocus}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,9 +5,11 @@ import Route from './Route';
 import SignIn from '../pages/Signin';
 import SignUp from '../pages/Signup';
 
-import Dashboard from '../pages/Dashboard';
 import ForgotPassword from '../pages/ForgotPassword';
 import ResetPassword from '../pages/ResetPassword';
+
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 const Routes: React.FC = () => (
   <Switch>
@@ -17,6 +19,7 @@ const Routes: React.FC = () => (
     <Route path="/reset-password" exact component={ResetPassword} />
 
     <Route path="/dashboard" component={Dashboard} isPrivate />
+    <Route path="/profile" component={Profile} isPrivate />
   </Switch>
 );
 
