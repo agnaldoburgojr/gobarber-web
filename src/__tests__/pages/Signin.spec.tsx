e1d9b29dd5897fa408656935940810b2763bbf11
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-10-28 11:07:10 -0300

    test: add errors tests to signin

--- src/__tests__/pages/Signin.spec.tsx
+++ src/__tests__/pages/Signin.spec.tsx
@@ -5,6 +5,8 @@ import {fireEvent} from '@testing-library/dom'
 import Signin from '../../pages/Signin'
 
 const mockedHistoryPush = jest.fn()
+const mockedSignin= jest.fn()
+const mockedAddToast = jest.fn()
 
 jest.mock('react-router-dom', ()=> {
   return {
@@ -18,12 +20,23 @@ jest.mock('react-router-dom', ()=> {
 jest.mock('../../hooks/auth', ()=> {
   return {
     useAuth: () => ({
-      signIn: jest.fn()
+      signIn: mockedSignin
+    })
+  }
+})
+
+jest.mock('../../hooks/toast', ()=> {
+  return {
+    useToast: () => ({
+      addToast: mockedAddToast
     })
   }
 })
 
 describe('Signin Page', () => {
+  beforeEach(()=> {
+    mockedHistoryPush.mockClear();
+  })
   it('should be able to sing in', async () => {
     const { getByPlaceholderText, getByText } = render(<Signin/>);
 
@@ -41,5 +54,44 @@ describe('Signin Page', () => {
     )
 
 
+  });
+
+  it('should not be able to sing in with invalid credentials', async () => {
+    const { getByPlaceholderText, getByText } = render(<Signin/>);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha')
+    const buttonElement = getByText('Entrar')
+
+    fireEvent.change(emailField, {target: {value: 'not-valid-email'}})
+    fireEvent.change(passwordField, {target: {value: '123456'}})
+
+    fireEvent.click(buttonElement)
+
+    await wait(()=>
+      expect(mockedHistoryPush).not.toHaveBeenCalled()
+    )
+  });
+
+  it('should display an error if login fails', async () => {
+
+    mockedSignin.mockImplementation(()=> {
+      throw new Error()
+    })
+
+    const { getByPlaceholderText, getByText } = render(<Signin/>);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha')
+    const buttonElement = getByText('Entrar')
+
+    fireEvent.change(emailField, {target: {value: 'johndoe@example.com'}})
+    fireEvent.change(passwordField, {target: {value: '123456'}})
+
+    fireEvent.click(buttonElement)
+
+    await wait(()=>
+      expect(mockedAddToast).toHaveBeenCalledWith(expect.objectContaining({type: 'error'}))
+    )
   });
 });
